9d5eb27c846427ba16f2f492f42512f889f14444
Vijay Rajasekar <vijayrajasekar1@gmail.com>
2020-08-23 03:14:05 -0400

    pls

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,14 +2,14 @@ export default {
 	mode: 'universal',
 	target: 'static',
 	head: {
-		title: 'Vijay Rajasekar | Software Engineer',
+		title: 'Vijay Rajasekar',
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{
 				hid: 'description',
 				name: 'description',
-				content: process.env.npm_package_description || ''
+				content: 'Software Engineer'
 			}
 		],
 		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
